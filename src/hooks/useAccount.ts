e71c2456dfd8b83649ee4e52a8d47161465e3319
Rostyslav <rostikkononenko0910@gmail.com>
2023-01-09 18:04:17 +0300

    Fixed fetching profile

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -4,21 +4,22 @@ import { useNavigate } from 'react-router-dom';
 import { useLazyGetUserQuery } from 'store/api';
 
 const useAccount = (): [IUser | null, () => void] => {
-	const [fetchUser, { data }] = useLazyGetUserQuery();
+	const [fetchUser, { error }] = useLazyGetUserQuery();
 	const [user, setUser] = useState<IUser | null>(null);
 	const navigate = useNavigate();
 
 	useEffect(() => {
 		if (localStorage.getItem('token')) {
-			fetchUser();
+			fetchUser().unwrap().then((res) => {
+				if (error) {
+					localStorage.removeItem('token');
+					navigate('/login');
+				} else {
+					setUser(res);
+				}
+			});
 		}
 	}, [localStorage.getItem('token')]);
-
-	useEffect(() => {
-		if (data) {
-			setUser(data);
-		}
-	}, [data]);
 
 	const logout = () => {
 		localStorage.removeItem('token');
